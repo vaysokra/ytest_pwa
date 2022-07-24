87333d1b57124d1c9dd384d5b14dbd14790b2fa6
vaysokragood123@gmail.com <vaysokragood123@gmail.com>
2022-07-24 18:01:17 +0700

    2mudblazor

---
+++ Models/dynamic.cs
@@ -0,0 +1,12 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ytest_pwa.Models
+{
+    public class dynamic
+    {
+        public string Name { get; set; }
+    }
+}
